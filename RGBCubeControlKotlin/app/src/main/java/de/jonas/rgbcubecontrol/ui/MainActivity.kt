01bc52e4f1d2f88828757c3e2fc6093782b23c99
Jonas Moerschbach <jmoerschbach@gmail.com>
2018-11-17 20:56:49 +0100

    updated plugins and dependencies

---
+++ RGBCubeControlKotlin/app/src/main/java/de/jonas/rgbcubecontrol/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.jonas.rgbcubecontrol.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import de.jonas.rgbcubecontrol.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
