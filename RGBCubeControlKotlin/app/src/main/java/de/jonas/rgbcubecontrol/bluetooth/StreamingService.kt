415a1f174f56a91f7a6424914c4ee63ed082f13b
Jonas Moerschbach <jmoerschbach@gmail.com>
2018-11-23 23:01:59 +0100

    problem now understood, how to get the bt into the service?

--- RGBCubeControlKotlin/app/src/main/java/de/jonas/rgbcubecontrol/bluetooth/StreamingService.kt
+++ RGBCubeControlKotlin/app/src/main/java/de/jonas/rgbcubecontrol/bluetooth/StreamingService.kt
@@ -6,21 +6,21 @@ import android.util.Log
 import app.akexorcist.bluetotohspp.library.BluetoothSPP
 import app.akexorcist.bluetotohspp.library.BluetoothState
 import de.jonas.rgbcubecontrol.ui.App
+import app.akexorcist.bluetotohspp.library.BluetoothSPP.BluetoothConnectionListener
+
 
 class StreamingService() : IntentService("StreamingService") {
-    private val bt: BluetoothSPP = BluetoothSPP(App.instance)
-    override fun onHandleIntent(p0: Intent?) {
 
+    override fun onHandleIntent(p0: Intent?) {
         Log.w("StreamingService", "onHandleIntent")
-        for (i in 0..10)
-            bt.send("Hallo", true)
+
+        Log.w("StreamingService", "going to send because connection sucessful")
+//        bt.send("Hallo", true)
     }
 
     override fun onCreate() {
         super.onCreate()
         Log.w("StreamingService", "onCreate")
-        bt.setupService()
-        bt.startService(BluetoothState.DEVICE_OTHER)
-        bt.autoConnect("Jonas")
+
     }
 }
--- RGBCubeControlKotlin/app/src/main/java/de/jonas/rgbcubecontrol/ui/MainActivity.kt
+++ RGBCubeControlKotlin/app/src/main/java/de/jonas/rgbcubecontrol/ui/MainActivity.kt
@@ -12,24 +12,40 @@ import de.jonas.rgbcubecontrol.R
 import app.akexorcist.bluetotohspp.library.DeviceList
 import android.content.Intent
 import android.app.Activity
+import android.widget.Button
 import de.jonas.rgbcubecontrol.bluetooth.StreamingService
 
 
 class MainActivity() : AppCompatActivity() {
-
+    private val bt: BluetoothSPP = BluetoothSPP(App.instance)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        if (!bt.isBluetoothEnabled()) {
-//            //findViewById<R.id.>()
-//        } else {
-//            bt.setupService()
-//            bt.startService(BluetoothState.DEVICE_OTHER)
-//            bt.autoConnect("Jonas")
-////            val intent = Intent(applicationContext, DeviceList::class.java)
-////            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE)
-//        }
+        if (!bt.isBluetoothEnabled()) {
+            //findViewById<R.id.>()
+        } else {
+            bt.setupService()
+            bt.startService(BluetoothState.DEVICE_OTHER)
+            bt.autoConnect("Jonas")
+            bt.setBluetoothConnectionListener(object : BluetoothSPP.BluetoothConnectionListener {
+                override fun onDeviceConnected(name: String, address: String) {
+                    Toast.makeText(App.instance, "enabling button", Toast.LENGTH_SHORT).show()
+                    Log.w("StreamingService", "succesfull connected to {$name}")
+                    findViewById<Button>(R.id.sendButton).isEnabled = true
+                }
+
+                override fun onDeviceDisconnected() {
+                    findViewById<Button>(R.id.sendButton).isEnabled = false
+                }
+
+                override fun onDeviceConnectionFailed() {
+                    findViewById<Button>(R.id.sendButton).isEnabled = false
+                }
+            })
+//            val intent = Intent(applicationContext, DeviceList::class.java)
+//            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE)
+        }
     }
 
     fun send(view: View) {
